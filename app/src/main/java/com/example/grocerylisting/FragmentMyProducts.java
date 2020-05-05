aaf3d68a772792ee055c40aa9c49330d2c206e9f
AKartashov <33695521+AlexanKartashov@users.noreply.github.com>
2020-05-05 23:20:16 +0400

    first commit, implement tabs

---
+++ app/src/main/java/com/example/grocerylisting/FragmentMyProducts.java
@@ -0,0 +1,26 @@
+package com.example.grocerylisting;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentMyProducts extends Fragment {
+
+    View view;
+
+    public FragmentMyProducts() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.myproducts_fragment,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/grocerylisting/FragmentSelectedRecipes.java
@@ -0,0 +1,26 @@
+package com.example.grocerylisting;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentSelectedRecipes extends Fragment {
+
+    View view;
+
+    public FragmentSelectedRecipes() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.selectedrecipes_fragment,container,false);
+        return view;
+    }
+}
